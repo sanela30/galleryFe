ca11cc127c0212b3da244fa204205fda7dac3124
Sanela30 <sanela26@gmail.com>
2018-02-22 00:32:18 +0100

    Register!

--- src/app/component/auth/register/register.component.ts
+++ src/app/component/auth/register/register.component.ts
@@ -15,31 +15,47 @@ import { AuthService } from '../../../shared/service/auth.service';
 export class RegisterComponent{
 
 	user = new User();
+	public errors: any[] = [];
 
   constructor(
     private authService: AuthService,
   	private router: Router) {
-  }
+	}
+	
+	public register(){
+    this.authService.register(
+					this.user.firstName, 
+			  	this.user.lastName, 
+					this.user.email, 
+					this.user.password,
+					this.user.password_confirmation)
+      .subscribe((user)=> {
+        this.router.navigateByUrl('/login');
+      },
+        (err: HttpErrorResponse) => {
+          alert(`Backend returned code ${err.status} with message: ${err.error}`);
+        }
+      );
+}
 
-   register() {
-   	this.authService.register(
-  		this.user.firstName, 
-   		this.user.lastName, 
-  		this.user.email, 
-			 this.user.password,
-			 this.user.password_confirmation
-   	).subscribe(
-   		(user) => {
-				 console.log()
-				this.authService.login(user.email, user.password).subscribe(
-					(user) => {
-						this.router.navigateByUrl('/');
-					}
-				)
-   			},
-	 	    (err: HttpErrorResponse) => {
-	 	    	alert(`${err.error.message}`);
-	 	    }
-   	);
-   }
+  //  register() {
+  //  	this.authService.register(
+  // 		this.user.firstName, 
+  //  		this.user.lastName, 
+  // 		this.user.email, 
+	// 		 this.user.password,
+	// 		 this.user.password_confirmation
+  //  	).subscribe(
+  //  		(user) => {	
+	// 			this.authService.login(user.email, user.password).subscribe(
+	// 				(user) => {
+	// 					this.router.navigateByUrl('/login');
+	// 				}
+	// 			)
+  //  			},
+	//  	    (err: HttpErrorResponse) => {
+	//  	    	alert(`${err.error.message}`);
+	//  	    }
+  //  	);
+  //  }
 }
